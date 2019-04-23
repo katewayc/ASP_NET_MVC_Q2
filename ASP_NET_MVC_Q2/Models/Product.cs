2e100cf408773906c2f2e533541c1e52ee837107
katewayc <kate.bqool@gmail.com>
2019-04-23 10:14:11 +0800

    add display annotations

--- ASP_NET_MVC_Q2/Models/Product.cs
+++ ASP_NET_MVC_Q2/Models/Product.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_NET_MVC_Q2.Models
 {
     public class Product
     {
+        [Display(Name= "產品唯一代號")]
         public int Id { get; set; }
+
+        [Display(Name = "地區")]
         public string Locale { get; set; }
+
+        [Display(Name = "產品名稱")]
         public string Product_Name { get; set; }
+
+        [Display(Name = "價格")]
         public decimal Price { get; set; }
+
+        [Display(Name = "促銷價格")]
         public string Promote_Price { get; set; }
+
+        [Display(Name = "建立時間")]
         public DateTime Create_Date { get; set; }
     }
 }
